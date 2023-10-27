a74d10e694e80c8e60b2bdbc09c5bc6a9fd303ff
bkubick <bjk2158@columbia.edu>
2023-10-26 20:10:16 -0500

    Add util module and house for prompt generator util

---
+++ startup-idea-generator/src/utils/prompt-generator.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview This file contains the prompt generator function that will be used to generate the prompts
+ * to be used in conjunction with the AI model.
+ */
+
+
+function generateIdeaPrompt(): string {
+    return '';
+}
