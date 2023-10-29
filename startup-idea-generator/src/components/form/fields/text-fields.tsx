1e6dc31248ef103d8b7614c5ab93bee506fbcd6c
bkubick <bjk2158@columbia.edu>
2023-10-29 13:02:56 -0500

    Fix typing for the text fields

--- startup-idea-generator/src/components/form/fields/text-fields.tsx
+++ startup-idea-generator/src/components/form/fields/text-fields.tsx
@@ -3,13 +3,32 @@ import React from 'react';
 
 /**
  * Interface for text fields.
+ * 
+ * @property {string} name The name of the text field.
+ * @property {string} value The value of the text field.
+ * @property {(event: React.ChangeEvent<any>) => void} onChange The change handler for the text field.
+ * @property {(event: React.FocusEvent<any>) => void} onBlur The blur handler for the text field.
  */
 interface TextField {
     name: string;
     value?: string;
-    className?: string;
     onChange: (event: React.ChangeEvent<any>) => void;
     onBlur: (event: React.FocusEvent<any>) => void;
+}
+
+
+/**
+ * Interface for text field props used in the formik form.
+ * 
+ * @property {TextField} field The text field.
+ * @property {string} className The class name for the text field.
+ * @property {string} placeholder The placeholder for the text field.
+ * @property {string} type The type for the text field.
+ * @property {(value: string | undefined) => string | undefined} validate The validation function for the text field.
+ */
+interface TextFieldArgs {
+    field: TextField;
+    className?: string;
     placeholder?: string;
     type?: string;
     validate?: (value: string | undefined) => string | undefined;
@@ -19,12 +38,12 @@ interface TextField {
 /**
  * Field component for input fields.
  * 
- * @param field
- * @param props
+ * @param field the text field information.
+ * @param props the optional props for the text field.
  * @returns React.JSX.Element with the custom input field
  */
-const InputField = ({ field, ...props }: { field: TextField, props: object }): React.JSX.Element => {
-    const className = 'w-full rounded-lg pl-2 py-1 bg-transparent ' + (field.className ? field.className : '');
+const InputField = ({ field, ...props }: TextFieldArgs): React.JSX.Element => {
+    const className = 'w-full rounded-lg pl-2 py-1 bg-transparent ' + (props.className ? props.className : '');
     return (
         <div className='border-slate-400 border-2 rounded-lg'>
             <input { ...field } { ...props } className={ className } />
@@ -36,12 +55,12 @@ const InputField = ({ field, ...props }: { field: TextField, props: object }): R
 /**
  * Field component for textarea fields.
  * 
- * @param field
- * @param props
+ * @param field the text field information.
+ * @param props the optional props for the text field.
  * @returns React.JSX.Element with the custom textarea field
  */
-const TextareaField = ({ field, ...props }: { field: TextField, props: object }): React.JSX.Element => {
-    const className = 'w-full rounded-lg pl-2 py-1 bg-transparent ' + (field.className ? field.className : '');
+const TextareaField = ({ field, ...props }: TextFieldArgs): React.JSX.Element => {
+    const className = 'w-full rounded-lg pl-2 py-1 bg-transparent ' + (props.className ? props.className : '');
     return (
         <div className='border-slate-400 border-2 rounded-lg'>
             <textarea { ...field } { ...props } className={ className } />
