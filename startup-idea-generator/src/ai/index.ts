3bd459637052dc5f1232dad56acbef9caad1404f
bkubick <bjk2158@columbia.edu>
2023-11-02 14:37:40 -0500

    Set the ai directory as a module

---
+++ startup-idea-generator/src/ai/index.ts
@@ -0,0 +1,12 @@
+/**
+ * This file is the entry point for the AI module, which contains
+ * various AI models that will be used to generate startup ideas.
+ * 
+ * Current Models:
+ * - ChatGPT: a request handler to the ChatGPT API, allowing for LLM generation.
+ */
+
+import { ChatGPT } from "./chatgpt.model";
+
+
+export { ChatGPT };
